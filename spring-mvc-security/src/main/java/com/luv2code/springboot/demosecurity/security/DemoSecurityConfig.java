88b1fd9689c0a191e264d271a668053a88ad03f5
tahmid-saj <tahmid.saj@gmail.com>
2023-09-23 21:50:50 -0400

    update userdetailsmanager in config

--- spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -7,11 +7,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class DemoSecurityConfig {
+    // add support for JDBC ... no more hardcoded users
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -34,6 +45,7 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+    */
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -57,4 +69,6 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
+
+
 }
